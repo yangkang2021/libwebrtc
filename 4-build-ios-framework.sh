b4c597c968e9ec24796c272d446353689e2499a8
yangkang <xiaomo.yk@gmail.com>
2022-07-10 17:38:05 +0800

    run on mac

--- 4-build-ios-framework.sh
+++ 4-build-ios-framework.sh
@@ -5,7 +5,7 @@ base_dir=$(cd "$(dirname "$0")";pwd)
 export PATH="$base_dir/depot_tools:$PATH"
 
 cd src
-./tools_webrtc/ios/build_ios_libs.py \
+python3 ./tools_webrtc/ios/build_ios_libs.py \
 --arch arm64 \
 --verbose \
 --extra-gn-args 'treat_warnings_as_errors=false  rtc_use_h264=false'
--- 4-build-mac.sh
+++ 4-build-mac.sh
@@ -4,5 +4,7 @@ base_dir=$(cd "$(dirname "$0")";pwd)
 
 export PATH="$base_dir/depot_tools:$PATH"
 
+alias python='vpython3'
+
 ninja -j4 -C src/out_mac_release webrtc  WebRTC.framework # all
 
